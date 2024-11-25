1468e214a219d93f1167f31d8fd0707b500a5701
ALADETAN-IFE <163364168+ALADETAN-IFE@users.noreply.github.com>
2024-11-25 10:40:38 +0100

    Added the welcome routes

--- router/routes.js
+++ router/routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const {signUp, login, addTask, 
-    getUser, getAllTask, getTask} = require("../controllers/controls")
+const { welcome, 
+    signUp, login, addTask, 
+    getUser, getAllTask, getTask } = require("../controllers/controls")
 const auth = require("../middleware/auth")
 
 const routes = express.Router()
@@ -8,6 +9,7 @@ const routes = express.Router()
 routes.post(`/signup`, signUp)
 routes.post(`/signin`, login)
 routes.post(`/add-task`, auth, addTask)
+routes.get(`/`, welcome)
 routes.get(`/:userId`, getUser)
 routes.get(`/tasks/:userId`, getAllTask)
 routes.get(`/task/:taskId`, getTask)
